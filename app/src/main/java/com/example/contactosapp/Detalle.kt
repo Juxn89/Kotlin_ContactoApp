13d7969ab5358331458251bb9cd1895d0964ff7f
= <=>
2020-03-10 14:24:08 -0600

    Se afina el diseño de la interfaz de activity_detalle

--- app/src/main/java/com/example/contactosapp/Detalle.kt
+++ app/src/main/java/com/example/contactosapp/Detalle.kt
@@ -2,11 +2,15 @@ package com.example.contactosapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class Detalle : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
